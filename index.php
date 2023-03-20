39a6c386f63a570419f99de327445d691769d164
issam <issam.azarkane@gmail.com>
2023-03-20 14:38:30 +0100

    Front - Navbar + Header

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
 <?php require_once "./src/required_pages/header.php" ?>
+<?php require_once "./src/required_pages/navbar.php" ?>
 
 
+<div class="container ">
+    <div class="container-fluid">
+        <div class="row" style="height: 500px">
+            <div class="col-md-12 col-lg-6  bg-image-left bg-cover rounded-start-4">
 
+                <div class="d-flex align-items-start flex-column mb-3" style="height: 500px;">
+                    <div class="mb-auto p-2 mt-3 w-100">
+                        <span class="bg-light px-3 py-2 rounded-4"><i class="fa-regular fa-bookmark me-2"></i>Hot
+                            recipes</span>
+                        <span class="bg-success px-3 py-2 rounded-4 text-light ms-2 bg-opacity-75"><i
+                                class="fa-solid fa-utensils me-2"></i>Chicken</span>
+                        <span class="bg-success px-3 py-2 rounded-4 text-light ms-2 bg-opacity-75"><i
+                                class="fa-solid fa-clock me-2"></i>30 Minutes</span>
+                        <span class="d-block mt-4 text-white fs-4 rounded-3 shadow-md">Spicy delicious chicken wings</span>
+                    </div>
+                    <div class="d-flex flex-row w-100">
+                        <div class="p-2 d-flex align-items-center">
+                            <img style="height: 40px; width: 40px" src="./assets/images/home/banner-avatar.webp"
+                                class="rounded-circle" alt="...">
+                            <div class="ms-2">
+                                <span class="d-block text-light">John Smith</span>
+                                <span class="d-block text-secondary-emphasis">15 March 2023</span>
+                            </div>
+                        </div>
+                        <div class="p-2  d-flex align-items-center ms-auto text-light">
+                            <button type="button" class="btn btn-dark bg-dark px-5 py-2 bg-opacity-75 rounded-4 shadow-sm">View Recipes <i class="fa-solid fa-circle-play ms-1"></i></button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6 bg-image-right bg-cover rounded-end-4">
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php require_once "./src/required_pages/footer.php" ?>
---
+++ src/required_pages/navbar.php
@@ -0,0 +1,43 @@
+<div class="container-fluid shadow-sm p-3 mb-5 rounded">
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        CookMaster
+        <img src="/CookMaster/assets/images/logo.webp" alt="Logo" width="30" height="24" class="d-inline-block align-text-top ms-2">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Events</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Courses</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Subscription</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-right-to-bracket me-2"></i>Sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-regular fa-user me-2"></i>Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
