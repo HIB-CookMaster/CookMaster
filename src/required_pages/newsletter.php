05094df753a1106389d3b4f3d8082929be1a2732
issam <issam.azarkane@gmail.com>
2023-03-20 17:46:19 +0100

    home page

---
+++ src/required_pages/newsletter.php
@@ -0,0 +1,19 @@
+    <div class="align-items-start d-flex justify-content-center">
+        <div
+            class="mt-8 mb-5 w-75 bg-newsletter bg-cover d-flex flex-column flex-nowrap align-content-center justify-content-center align-items-center rounded-5">
+            <h2 class="text-white fw-bold">Deliciousness to your inbox</h2>
+            <p class="w-50 text-white opacity-50 text-center fst-normal">Lorem ipsum dolor sit amet, consectetuipisicing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
+
+            <div class="input-group w-50 px-2 py-2 bg-white rounded-4">
+                <form class="w-100" action="">
+                    <div class="d-flex">
+                    <input type="text" class="form-control border-0 active" placeholder="Entrez votre email ici...">
+                    <button class="btn btn-primary bg-black border-0 rounded-4 px-4 py-2" type="submit">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
