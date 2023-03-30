23d53ccc29b2b7cd595a616c5dd2e7ffdb83ec7d
issam <issam.azarkane@gmail.com>
2023-03-30 08:39:30 +0200

    Front

---
+++ src/pages/events.php
@@ -0,0 +1,286 @@
+<?php require_once "../required_pages/header.php" ?>
+<?php require_once "../required_pages/navbar.php" ?>
+
+<div class="container">
+    <h2 class="text-center mt-4">Events</h2>
+    <span class="opacity-75">
+        <p class="text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore</p>
+    </span>
+
+    <div class="row">
+        <div class="col-lg-3 shadow-sm border border-secondary-subtle p-0 overflow-x-scroll">
+            <div class="mt-4">
+                <span class="p-4 ">
+                    <span class="fs-3 opacity-50">This</span>
+                    <span class="fs-1 fw-bold">Week</span>
+                </span>
+            </div>
+
+            <div class="d-flex mt-4 bg-success p-4 bg-opacity-10">
+                <div class="d-date d-flex flex-column text-center">
+                    <span class="p-1 px-2 rounded-3 text-light bg-today">Today</span>
+                    <span class="fs-1 fw-bold">M</span>
+                    <span class="text-dark opacity-50">10th</span>
+                </div>
+                <div class="d-date d-flex flex-column ms-4">
+                    <span class="fw-semibold">Workshop - Cheese Cake</span>
+                    <span class="d-flex px-1 mt-2 opacity-25 align-items-center">
+                        <span class="px-little">11:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">12:00</span>
+                    </span>
+                    <span class="text-danger mt-2 mb-3">
+                        <i class="fa-solid fa-minus fa-2xl"></i>
+                        <span>Begins in 45m</span>
+                    </span>
+
+
+
+
+                    <span class="fw-semibold mt-2">Ergonomics</span>
+                    <span class="d-flex px-1 opacity-25 align-items-center">
+                        <span class="px-little mt-2">12:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">13:00</span>
+                    </span>
+                </div>
+            </div>
+
+            <div class="d-flex p-4">
+                <div class="d-date d-flex flex-column text-center">
+                    <span class="fs-1 fw-bold">T</span>
+                    <span class="text-dark opacity-50">11th</span>
+                </div>
+                <div class="d-date d-flex flex-column ms-4">
+                    <span class="fw-semibold">Workshop - Cheese Cake</span>
+                    <span class="d-flex px-1 mt-2 opacity-25 align-items-center">
+                        <span class="px-little">11:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">12:00</span>
+                    </span>
+                    <span class="fw-semibold mt-2">Ergonomics</span>
+                    <span class="d-flex px-1 opacity-25 align-items-center">
+                        <span class="px-little mt-2">12:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">13:00</span>
+                    </span>
+                </div>
+            </div>
+
+            <div class="d-flex p-4">
+                <div class="d-date d-flex flex-column text-center">
+                    <span class="fs-1 fw-bold">W</span>
+                    <span class="text-dark opacity-50">12th</span>
+                </div>
+                <div class="d-date d-flex flex-column ms-4">
+                    <span class="fw-semibold">Workshop - Cheese Cake</span>
+                    <span class="d-flex px-1 mt-2 opacity-25 align-items-center">
+                        <span class="px-little">11:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">12:00</span>
+                    </span>
+                    <span class="fw-semibold mt-2">Ergonomics</span>
+                    <span class="d-flex px-1 opacity-25 align-items-center">
+                        <span class="px-little mt-2">12:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">13:00</span>
+                    </span>
+                </div>
+            </div>
+
+            <div class="d-flex p-4">
+                <div class="d-date d-flex flex-column text-center">
+                    <span class="fs-1 fw-bold">T</span>
+                    <span class="text-dark opacity-50">11th</span>
+                </div>
+                <div class="d-date d-flex flex-column ms-4">
+                    <span class="fw-semibold">Workshop - Cheese Cake</span>
+                    <span class="d-flex px-1 mt-2 opacity-25 align-items-center">
+                        <span class="px-little">11:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">12:00</span>
+                    </span>
+                    <span class="fw-semibold mt-2">Ergonomics</span>
+                    <span class="d-flex px-1 opacity-25 align-items-center">
+                        <span class="px-little mt-2">12:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">13:00</span>
+                    </span>
+                </div>
+            </div>
+
+            <div class="d-flex p-4">
+                <div class="d-date d-flex flex-column text-center">
+                    <span class="fs-1 fw-bold">F</span>
+                    <span class="text-dark opacity-50">11th</span>
+                </div>
+                <div class="d-date d-flex flex-column ms-4">
+                    <span class="fw-semibold">Workshop - Cheese Cake</span>
+                    <span class="d-flex px-1 mt-2 opacity-25 align-items-center">
+                        <span class="px-little">11:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">12:00</span>
+                    </span>
+                    <span class="fw-semibold mt-2">Ergonomics</span>
+                    <span class="d-flex px-1 opacity-25 align-items-center">
+                        <span class="px-little mt-2">12:00</span>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <i class="fa-solid fa-circle px-1 fa-little"></i>
+                        <span class="px-little">13:00</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-9 shadow-sm border border-secondary-subtle overflow-y-scroll p-0" style="max-height: 1015px">
+            <img src="../../assets/images/events/banner.png" class="img-fluid" alt="">
+
+            <div class="d-flex border-bottom border-secondary-subtle">
+                <div class="me-auto p-2">
+                    <div class="p-3 ">
+                        <span class="">
+                            <span class="opacity-50">10th,</span>
+                            <span class="fw-bold fs-1"> Monday</span>
+                            <i class="fa-regular fa-calendar text-info-emphasis fa-2xl ms-3"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="p-2 bg-dark bg-opacity-10 text-dark fw-semibold opacity-75 rounded-3 mx-2 my-2 h-25">21:34
+                </div>
+            </div>
+
+            <div class="mt-2 p-3">
+                <span class="opacity-50">Begins in 45 Minutes </span>
+                <i class="fa-solid fa-clock text-danger ms-2 opacity-75"></i>
+            </div>
+
+            <div class="row m-0 p-1 border-bottom border-secondary-subtle">
+                <div class="card shadow-none border-0 p-2 d-flex col-lg-6 col-sm-12">
+                    <div class="card-body">
+                        <h5>Workshop - Cheese Cake <span class="badge bg-secondary bg-opacity-10 ms-2 fw-normal text-dark opacity-50"><i class="fa-solid fa-user fa-2xs me-2"></i>28/31</span></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"><i class="fa-solid fa-location-dot me-2 text-success"></i>Le Comptoir du Relais - 9 Carrefour de l'Odéon, 75006 Paris</h6>
+                        <img src="../../assets/images/events/event1.png" alt="" class="img-fluid rounded-4 shadow-sm">
+                        <div class="d-flex">
+                            <div class="mt-3">
+                                <div>
+                                    <i class="fa-solid fa-clock me-1 text-success"></i> 
+                                    <span class="opacity-75 text-dark">Today</span>
+                                </div>
+
+                                <div class="mt-2 opacity-75 text-dark fs-little">From 11:00 am to 12:00 pm</div>
+                            </div>
+                            <div class="d-flex align-items-center ms-auto pointer">
+                                <div class="text-primary-emphasis me-3"><i class="fa-solid fa-share me-3"></i><span>More infos</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 mx-auto mb-3">
+                    <img src="../../assets/images/events/map.png" class="img-fluid rounded-3" alt="">
+                </div>
+            </div>
+
+            <div class="mt-2 p-3">
+                <span class="opacity-75 fs-2">Next events</span>
+                <i class="fa-solid fa-clock text-success ms-2 opacity-75 fa-xl"></i>
+            </div>
+
+            <div class="row m-0 p-1 border-bottom border-secondary-subtle">
+                <div class="card shadow-none border-0 p-2 d-flex col-lg-6 col-sm-12">
+                    <div class="card-body">
+                        <h5>Workshop - Cheese Cake <span class="badge bg-secondary bg-opacity-10 ms-2 fw-normal text-dark opacity-50"><i class="fa-solid fa-user fa-2xs me-2"></i>28/31</span></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"><i class="fa-solid fa-location-dot me-2 text-success"></i>Le Comptoir du Relais - 9 Carrefour de l'Odéon, 75006 Paris</h6>
+                        <img src="../../assets/images/events/event1.png" alt="" class="img-fluid rounded-4 shadow-sm">
+                        <div class="d-flex">
+                            <div class="mt-3">
+                                <div>
+                                    <i class="fa-solid fa-clock me-1 text-success"></i> 
+                                    <span class="opacity-75 text-dark">Today</span>
+                                </div>
+
+                                <div class="mt-2 opacity-75 text-dark fs-little">From 11:00 am to 12:00 pm</div>
+                            </div>
+                            <div class="d-flex align-items-center ms-auto pointer">
+                                <div class="text-primary-emphasis me-3"><i class="fa-solid fa-share me-3"></i><span>More infos</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card shadow-none border-0 p-2 d-flex col-lg-6 col-sm-12">
+                    <div class="card-body">
+                        <h5>Workshop - Cheese Cake <span class="badge bg-secondary bg-opacity-10 ms-2 fw-normal text-dark opacity-50"><i class="fa-solid fa-user fa-2xs me-2"></i>28/31</span></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"><i class="fa-solid fa-location-dot me-2 text-success"></i>Le Comptoir du Relais - 9 Carrefour de l'Odéon, 75006 Paris</h6>
+                        <img src="../../assets/images/events/event1.png" alt="" class="img-fluid rounded-4 shadow-sm">
+                        <div class="d-flex">
+                            <div class="mt-3">
+                                <div>
+                                    <i class="fa-solid fa-clock me-1 text-success"></i> 
+                                    <span class="opacity-75 text-dark">Today</span>
+                                </div>
+
+                                <div class="mt-2 opacity-75 text-dark fs-little">From 11:00 am to 12:00 pm</div>
+                            </div>
+                            <div class="d-flex align-items-center ms-auto pointer">
+                                <div class="text-primary-emphasis me-3"><i class="fa-solid fa-share me-3"></i><span>More infos</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php require_once "../required_pages/footer.php" ?>
--- src/required_pages/navbar.php
+++ src/required_pages/navbar.php
@@ -3,9 +3,11 @@
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <span class="logo-title fw-bold">CookMaster</span>
-        <img src="/CookMaster/assets/images/logo.webp" alt="Logo" width="30" height="24" class="d-inline-block align-text-top ms-2">
+        <img src="/CookMaster/assets/images/logo.webp" alt="Logo" width="30" height="24"
+          class="d-inline-block align-text-top ms-2">
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
@@ -14,13 +16,33 @@
             <a class="nav-link" href="/CookMaster/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/CookMaster/src/pages/courses.php">Courses</a>
+          <div class="dropdown">
+              <a class="dropdown-toggle nav-link" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Formations
+              </a>
+
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/CookMaster/src/pages/courses.php">Courses</a></li>
+                <li><a class="dropdown-item" href="#">Professional training</a></li>
+              </ul>
+            </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Events</a>
+            <a class="nav-link" href="/CookMaster/src/pages/events.php">Events</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Shop</a>
+            <div class="dropdown">
+              <a class="dropdown-toggle nav-link" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Shop
+              </a>
+
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Kitchen equipment</a></li>
+                <li><a class="dropdown-item" href="#">Home tutoring</a></li>
+              </ul>
+            </div>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/CookMaster/src/pages/subscription.php">Subscription</a>
@@ -28,7 +50,8 @@
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="/CookMaster/src/pages/signup.php"><i class="fa-solid fa-right-to-bracket me-2"></i>Sign up</a>
+            <a class="nav-link" href="/CookMaster/src/pages/signup.php"><i
+                class="fa-solid fa-right-to-bracket me-2"></i>Sign up</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/CookMaster/src/pages/login.php"><i class="fa-regular fa-user me-2"></i>Login</a>
